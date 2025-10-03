220be6cb1bf53cafd0ddb19fade58dd921a7f752
Purple115 <89314203+Purple115@users.noreply.github.com>
2025-10-03 14:35:08 +0200

    Add files via upload

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -1,63 +1,132 @@
 package no.hvl.dat100.tabeller;
 
+import java.util.Arrays;
+
 public class Tabeller {
+    public static void main(String[] args) {
+        int[] tabell = {19, 7, 4, 2, 11};
+        skrivUt(tabell);
 
-	// a)
-	public static void skrivUt(int[] tabell) {
+        tilStreng(tabell);
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+        summer(tabell);
 
-	}
+        int tall = 11;
+        finnesTall(tabell, tall);
 
-	// b)
-	public static String tilStreng(int[] tabell) {
+        posisjonTall(tabell, tall);
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-	}
+        reverser(tabell);
 
-	// c)
-	public static int summer(int[] tabell) {
+        erSortert(tabell);
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
-	}
+        int[] tabell2 = {1, 2, 1, 2, 11};
 
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
+        settSammen(tabell,  tabell2);
+    }
+    // a
+    public static void skrivUt(int[] tabell) {
+        for (int i = 0; i < tabell.length -1; i++ ) {
+            System.out.println(tabell[i]);
+        }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+    }
 
-	}
+    // b)
+    public static String tilStreng(int[] tabell) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int i = 0; i < tabell.length; i++) {
+            if (i > 0) sb.append(',');
+            sb.append(tabell[i]);
+        }
+        sb.append(']');
+        return sb.toString();
 
-	// e)
-	public static int posisjonTall(int[] tabell, int tall) {
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
-	}
+    // c)
+    public static int summer(int[] tabell) {
 
-	// f)
-	public static int[] reverser(int[] tabell) {
+        int n = 0;
+        int sum = 0;
+        while (n < tabell.length) {
+            sum = sum + tabell[n];
+            n++;
+        }
+        System.out.println(" ");
+        System.out.println(sum);
+        return sum;
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
-	}
+    // d)
+    public static boolean finnesTall(int[] tabell, int tall) {
 
-	// g)
-	public static boolean erSortert(int[] tabell) {
+        int j = 0;
+        boolean f = false;
+        while (j < tabell.length) {
+            if (tabell[j] == tall) {
+                f = true;
+                j = tabell.length;
+            } else {
+                j++;
+            }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
-	}
+        }
+        System.out.println(f);
+        return f;
+    }
 
-	// h)
-	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+    // e)
+    public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
+        for (int i = 0; i < tabell.length; i++) {
+            if (tabell[i] == tall) return i;
+        }
+        return -1;
+    }
+    // f)
+    public static int[] reverser(int[] tabell) {
+        int min = 0;
+        int max = tabell.length - 1;
+        int[] revers = new int[tabell.length];
 
-	}
+        while (min < tabell.length){
+            revers[max] = tabell[min];
+            min++;
+            max--;
+
+        }
+        for (int i = 0; i < revers.length; i++ ) {
+            System.out.println(revers[i]);
+        }
+        return revers;
+    }
+
+    // g)
+    public static boolean erSortert(int[] tabell) {
+        for (int i = 1; i < tabell.length; i++) {
+            if (tabell[i] < tabell[i - 1]) return false;
+        }
+        return true;
+    }
+
+    // h)
+    public static int[] settSammen(int[] tabell, int[] tabell2) {
+        int[] sammen = new int[tabell.length + tabell2.length];
+        int i = 0;
+        int j = 0;
+        while (i < tabell.length) {
+            sammen[i] = tabell[i];
+            System.out.println(sammen[i]);
+            i++;
+        }
+        while (j < tabell2.length) {
+            sammen[i] = tabell2[j];
+            System.out.println(sammen[i]);
+            i++;
+            j++;
+        }
+        return sammen;
+    }
 }
